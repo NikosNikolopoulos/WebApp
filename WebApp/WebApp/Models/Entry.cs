e5cd4e012ae5a7777def3c4da7db261fb1987add
nnikolopoulos <nnikolopoulos@systemorph.com>
2024-02-26 01:41:53 +0100

    Minimum Functioning App

---
+++ WebApp/WebApp/Models/Entry.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Models
+{
+    public class Entry
+    {
+        public int Id{ get; set; }
+        public string Question{ get; set; }
+        public string Answer{ get; set; }
+
+        public Entry()
+        {
+                
+        }
+    }
+}
