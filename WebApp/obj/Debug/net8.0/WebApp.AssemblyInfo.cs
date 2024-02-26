793e254d202626ec45dc4518860941ffb0d33079
nnikolopoulos <nnikolopoulos@systemorph.com>
2024-02-26 01:51:20 +0100

    minor

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cd4e012ae5a7777def3c4da7db261fb1987add")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb7e462af256fb721870cd43ccac47ee2a03225")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
